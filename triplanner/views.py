aaa8d7f38e8a06bb07cff3c2db4d7f4ec56d96ac
amazebluesky <amazebluesky@gmail.com>
2018-04-14 17:28:39 +0800

    a

--- triplanner/views.py
+++ triplanner/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    content = '<html>home</html>'
-
-    response = HttpResponse(content, content_type='text/html')
-    response['Content-Length'] = len(content)
-
-    return response
-
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('triplanner/home.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
