fd098ecda28438441654ed7ed8c539688a1f95f6
amazebluesky <amazebluesky@gmail.com>
2018-04-14 16:39:49 +0800

    a

--- randy/settings.py
+++ randy/settings.py
@@ -29,8 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 # Application definition
 
@@ -83,7 +82,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
