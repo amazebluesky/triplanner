8b4e7fdf3beae1ba3ec19b8726ecc18c271de99a
amazebluesky <amazebluesky@gmail.com>
2018-04-14 17:25:29 +0800

    a

--- triplanner/views.py
+++ triplanner/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def featured(request):
+def index(request):
     content = '<html>home</html>'
 
-    response = HttpResponse(content, content_type='application/liquid')
+    response = HttpResponse(content, content_type='text/html')
     response['Content-Length'] = len(content)
 
     return response
